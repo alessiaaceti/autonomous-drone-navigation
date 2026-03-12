828a1d070dd7cad550cc26504bbd1cf6726b1450
Alessia Aceti <acetialessia22@gmail.com>
2026-03-12 19:25:06 +0100

    Nodo publisher

--- drone_ws/src/drone_navigation_cpp/src/camera_viewer.cpp
+++ drone_ws/src/drone_navigation_cpp/src/camera_viewer.cpp
@@ -1,5 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
+#include <std_msgs/msg/int32.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
 
@@ -14,7 +15,10 @@ public:
             std::bind(&DroneVisionNode::image_callback, this, std::placeholders::_1)
         );
 
-        RCLCPP_INFO(this->get_logger(), "Nodo Target Tracking avviato. Cerco il bersaglio BLU...");
+        error_pub_ = this->create_publisher<std_msgs::msg::Int32>("/target_error_x", 10);
+
+        // ECCO LA FRASE GIUSTA!
+        RCLCPP_INFO(this->get_logger(), "Nodo Target Tracking avviato. Pubblico l'errore su /target_error_x");
     }
 
     ~DroneVisionNode() {
@@ -28,43 +32,39 @@ private:
             cv::Mat frame = cv_ptr->image;
             cv::Mat hsv, mask;
 
-            // 1. Convertiamo da BGR a HSV
             cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);
 
-            // 2. Definiamo i limiti del colore BLU (molto tolleranti per le ombre di Gazebo)
             cv::Scalar lower_blue(90, 50, 50);
             cv::Scalar upper_blue(150, 255, 255);
 
-            // 3. Creiamo la Maschera
             cv::inRange(hsv, lower_blue, upper_blue, mask);
 
-            // Puliamo il rumore
             cv::erode(mask, mask, cv::Mat(), cv::Point(-1, -1), 2);
             cv::dilate(mask, mask, cv::Mat(), cv::Point(-1, -1), 2);
 
-            // 4. Troviamo il centro di massa
             cv::Moments m = cv::moments(mask, true);
             
             int cx_screen = frame.cols / 2;
             int cy_screen = frame.rows / 2;
             
-            // Mirino centrale
             cv::drawMarker(frame, cv::Point(cx_screen, cy_screen), cv::Scalar(0, 0, 255), cv::MARKER_CROSS, 20, 2);
 
             if (m.m00 > 1000) { 
                 int cx_obj = m.m10 / m.m00;
                 int cy_obj = m.m01 / m.m00;
 
-                // Cerchio verde sul bersaglio
                 cv::circle(frame, cv::Point(cx_obj, cy_obj), 10, cv::Scalar(0, 255, 0), -1);
-
-                // Linea di errore
                 cv::line(frame, cv::Point(cx_screen, cy_screen), cv::Point(cx_obj, cy_obj), cv::Scalar(255, 0, 0), 2);
                 
                 int error_x = cx_obj - cx_screen;
                 
                 cv::putText(frame, "Tracking... Err X: " + std::to_string(error_x), 
                             cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 255, 0), 2);
+
+                std_msgs::msg::Int32 error_msg;
+                error_msg.data = error_x;
+                error_pub_->publish(error_msg);
+
             } else {
                 cv::putText(frame, "Target PERSO", 
                             cv::Point(10, 30), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(0, 0, 255), 2);
@@ -80,6 +80,7 @@ private:
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr error_pub_; 
 };
 
 int main(int argc, char * argv[]) {
